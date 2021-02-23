5d24ea5bf991a7310282ff03938eae8cdb6e00ae
sarahlee03 <sarahlee.elhadad@gmail.com>
2021-02-23 19:21:06 +0200

    change fab icon to add icon

--- app/src/main/java/com/example/readdit/MainActivity.java
+++ app/src/main/java/com/example/readdit/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.main_add_review_btn);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
