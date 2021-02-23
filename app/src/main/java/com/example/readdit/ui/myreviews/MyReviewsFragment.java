3bd9ac1ce8a3b53e123bf3aa28dcea2a4bdcc6df
sarahlee03 <sarahlee.elhadad@gmail.com>
2021-02-23 19:12:14 +0200

    create my reviews and profile fragments and add them to drawer

---
+++ app/src/main/java/com/example/readdit/ui/myreviews/MyReviewsFragment.java
@@ -0,0 +1,38 @@
+package com.example.readdit.ui.myreviews;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.readdit.R;
+
+public class MyReviewsFragment extends Fragment {
+
+    private MyReviewsViewModel mViewModel;
+
+    public static MyReviewsFragment newInstance() {
+        return new MyReviewsFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.my_reviews_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(MyReviewsViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
