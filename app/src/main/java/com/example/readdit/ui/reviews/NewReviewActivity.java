65cf7ad5f584098412e9d8f5d04a1cf0e8285dd9
shahar freidenberg <shahar.freidenberg@gmail.com>
2021-04-13 19:54:54 +0300

    new review activity design

--- app/src/main/java/com/example/readdit/ui/reviews/NewReviewActivity.java
+++ app/src/main/java/com/example/readdit/ui/reviews/NewReviewActivity.java
@@ -12,6 +12,8 @@ import com.example.readdit.SignUpActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import static com.example.readdit.R.layout.new_review_activity;
+
 public class NewReviewActivity extends AppCompatActivity {
 
     @Override
@@ -22,7 +24,22 @@ public class NewReviewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.new_review_activity);
+        setContentView(new_review_activity);
+        Button btnSave = findViewById(R.id.newreview_save_button);
+        Button btnCancel = findViewById(R.id.newreview_cancel_button);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // save review
+                finish();
+            }
+        });
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 
 }
