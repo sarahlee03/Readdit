60a2cd13d90287a0786af4c645f4b2efc5efb2f0
shahar freidenberg <shahar.freidenberg@gmail.com>
2021-04-16 23:45:01 +0300

    add my reviews ui

---
+++ app/src/main/java/com/example/readdit/ui/myreviews/MyReviewsAdapter.java
@@ -0,0 +1,54 @@
+package com.example.readdit.ui.myreviews;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.readdit.R;
+import com.example.readdit.model.Review;
+
+import java.util.List;
+
+public class MyReviewsAdapter extends RecyclerView.Adapter<MyReviewsViewHolder>{
+    LayoutInflater inflater;
+    MyReviewsViewModel viewModel;
+
+    public MyReviewsAdapter(LayoutInflater inflater, MyReviewsViewModel viewModel){
+        this.inflater = inflater;
+        this.viewModel = viewModel;
+    }
+    public interface OnItemClickListener{
+        void onItemClick(int position);
+    }
+
+    private OnItemClickListener listener;
+
+    public void setOnClickListener(OnItemClickListener listener){
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public MyReviewsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.review_row,parent,false);
+        MyReviewsViewHolder holder = new MyReviewsViewHolder(view);
+        holder.listener = listener;
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyReviewsViewHolder holder, int position) {
+        Review review = viewModel.getMyReviews().getValue().get(position);
+
+        holder.bindData(review, position);
+    }
+
+    @Override
+    public int getItemCount() {
+        if(viewModel.getMyReviews().getValue() != null) { return viewModel.getMyReviews().getValue().size(); } return 0;
+    }
+}
---
+++ app/src/main/java/com/example/readdit/ui/myreviews/MyReviewsViewHolder.java
@@ -0,0 +1,58 @@
+package com.example.readdit.ui.myreviews;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.readdit.R;
+import com.example.readdit.model.Review;
+import com.squareup.picasso.Picasso;
+
+public class MyReviewsViewHolder extends RecyclerView.ViewHolder{
+    public MyReviewsAdapter.OnItemClickListener listener;
+    TextView book;
+    TextView author;
+    TextView category;
+    TextView date;
+    RatingBar rating;
+    TextView username;
+    ImageView image;
+    ImageView userImage;
+    int position;
+
+    public MyReviewsViewHolder(@NonNull View itemView) {
+        super(itemView);
+        book = itemView.findViewById(R.id.review_name_tv);
+        author = itemView.findViewById(R.id.review_author_tv);
+        category = itemView.findViewById(R.id.review_category_tv);
+        date = itemView.findViewById(R.id.review_date_tv);
+        rating = itemView.findViewById(R.id.review_ratingbar);
+        username = itemView.findViewById(R.id.review_username_tv);
+        image = itemView.findViewById(R.id.review_bookimage);
+        userImage = itemView.findViewById(R.id.drawer_profile_img);
+
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.onItemClick(position);
+            }
+        });
+    }
+
+    public void bindData(Review review, int position) {
+        book.setText(review.getBook());
+        author.setText(review.getAuthor());
+        category.setText(review.getCategory());
+        date.setText(review.getDate());
+        rating.setRating((float)review.getRating());
+        username.setText(review.getUsername());
+        if(review.getImage() != null) { Picasso.get().load(review.getImage()).placeholder(R.drawable.book_placeholder).into(image); }
+        if(review.getUserImage() != null) { Picasso.get().load(review.getUserImage()).placeholder(R.drawable.profile_placeholder).into(userImage); }
+        this.position = position;
+    }
+}
